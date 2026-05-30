ff6d151ddac1b2fd935c9c637e58610e823c159a
techieayusshh <ayushraj805111@gmail.com>
2026-05-30 20:59:55 +0530

    Tune gunicorn for Render timeouts

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+import os
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+workers = 1
+threads = 2
+worker_class = "gthread"
+timeout = 180
+graceful_timeout = 30
+keepalive = 5
+max_requests = 200
+max_requests_jitter = 50
